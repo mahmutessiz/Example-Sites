9ce990f52f95e514f9b142c4097c68525bdbf781
Mahmut Eşsiz <102971403+mahmutessiz@users.noreply.github.com>
2022-10-06 17:50:26 +0300

    Update app.js

--- basic pomodoro site/app.js
+++ basic pomodoro site/app.js
@@ -4,65 +4,40 @@ const seconds = document.querySelector('.seconds');
 const playButton = document.querySelector('.play');
 const pauseButton = document.querySelector('.pause');
 const stopButton = document.querySelector('.stop');
-/* const settingButton = document.querySelector('.settings');
-const settingButton2 = document.querySelector('.settings-2');
-
-const inputDiv = document.querySelector('.inputs-div');
-
-
-settingButton.addEventListener('click', () => {
-    
-    inputDiv.style.display = 'block';
-    settingButton2.style.display = 'block';
-    settingButton.style.display = 'none';
-
-});
-
-settingButton2.addEventListener('click', () => {
-
-    
-    settingButton2.style.display = 'none';
-    settingButton.style.display = 'block';
-    inputDiv.style.display = 'none';
-}); */
-
-
 
 function timer() {
-
     let minute = Number(minutes.innerText);
     let second = Number(seconds.innerText);
 
-
-    if (second > 00) {
-        second--;
-    }
-    if (second === 0) {
-        second = 59;
-        minute--;
-    }
-    if (second < 10) {
-        second = '0' + second;
-    }
-    if (minute < 10) {
-        minute = '0' + minute;
-    }
-    if (minute < 0) {
-
-        minute = '00';
-        second = "00";
-
+    if ((minute == 0 && second == 0)) {
+        minute = 0;
+        second = 0;
         pauseButton.style.display = 'none';
         playButton.style.display = 'block';
     }
-
-
-
+    else {
+        if (second > 0) {
+            second--;
+        }
+        else{
+            if (minute == 0 && second == 0) {}
+            else if (second == 0) {
+                second = 59;
+                minute--;
+            }
+        }
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
 
     minutes.innerHTML = minute;
     seconds.innerHTML = second;
-
 }
+
 
 
 playButton.addEventListener('click', () => {
